ec1356378f535a107a4b9375de164e4ca9fb1b1f
Gustavo Oliveira <gustavoamorim42@gmail.com>
2024-05-23 19:58:32 -0300

    feat: add welcome message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
-
+import { format } from "date-fns";
 import Header from "./components/header";
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
   return (
     <main>
-      <div>
-        <Header />
-      </div>
+      <header>
+        <div>
+          <Header />
+          <div className="py-6 px-5">
+            <h2 className="text-xl">Olá, <span className="font-bold">Gustavo</span>!</h2>
+            <p className="capitalize text-sm">
+              {format(new Date(), "EEEE',' dd 'de' LLLL", {locale: ptBR})}
+            </p>
+          </div>
+        </div>
+      </header>
+
     </main>
   );
 }
