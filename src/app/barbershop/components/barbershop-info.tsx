a6cc17d66e8df537024d3536c5a45a8748a7ed4f
Gustavo Oliveira <gustavoamorim42@gmail.com>
2024-06-06 13:48:49 -0300

    feat: initial BarbershopInfo component

---
+++ src/app/barbershop/components/barbershop-info.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { Button } from "@/app/components/ui/button";
+import { Card, CardContent } from "@/app/components/ui/card";
+import { Barbershop } from "@prisma/client";
+import { ChevronLeftIcon, MapPin, MenuIcon, StarIcon } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+interface BarbershopInfoProps {
+  barbershop: Barbershop;
+}
+
+const BarbershopInfo = ({ barbershop }: BarbershopInfoProps) => {
+  const router = useRouter();
+
+  const handleBack = () => {
+    router.back();
+  };
+  return (
+    <>
+      <section className="pb-6 border-b-2 border-solid border-secondary">
+        <div className="h-[250px] w-full relative">
+          <Button
+            onClick={handleBack}
+            size={"icon"}
+            variant={"outline"}
+            className="absolute z-50 top-6 left-6"
+          >
+            <ChevronLeftIcon size={18} />
+          </Button>
+          <Button
+            size={"icon"}
+            variant={"outline"}
+            className="absolute z-50 top-6 right-6"
+          >
+            <MenuIcon size={18} />
+          </Button>
+          <Image
+            src={barbershop.imageUrl}
+            alt={barbershop.name}
+            fill
+            style={{
+              objectFit: "cover",
+            }}
+            priority
+          />
+        </div>
+        <div className="pt-3 px-5">
+          <h2 className="font-bold text-xl">{barbershop.name}</h2>
+          <div className="flex items-center gap-2 text-sm mt-2">
+            <MapPin size={16} className="text-primary" />
+            {barbershop.address}
+          </div>
+          <div className="flex items-center gap-2 text-sm mt-2">
+            <StarIcon size={16} className="text-primary fill-primary" />{" "}
+            <span>5,0 (0 Avaliações)</span>
+          </div>
+        </div>
+      </section>
+      <section className="pt-6 px-5">
+        <div className="flex gap-3">
+          <Button className="">Serviços</Button>
+          <Button className="bg-secondary">Informações</Button>
+        </div>
+        <div className="pt-3">
+          <Card>
+            <CardContent>{/* <Image src={} /> */}</CardContent>
+          </Card>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default BarbershopInfo;
