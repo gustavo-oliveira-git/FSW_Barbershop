1752d3a1757ab6611becd6df74bb9ff7fc042dd7
Gustavo Oliveira <gustavoamorim42@gmail.com>
2024-06-17 22:15:33 -0300

    feat: add initial side menu component

--- src/app/barbershop/components/barbershop-info.tsx
+++ src/app/barbershop/components/barbershop-info.tsx
@@ -1,7 +1,8 @@
 "use client";
 
+import SideMenu from "@/app/components/side-menu";
 import { Button } from "@/app/components/ui/button";
-import { Card, CardContent } from "@/app/components/ui/card";
+import { Sheet, SheetContent, SheetTrigger } from "@/app/components/ui/sheet";
 import { Barbershop } from "@prisma/client";
 import { ChevronLeftIcon, MapPin, MenuIcon, StarIcon } from "lucide-react";
 import Image from "next/image";
@@ -29,13 +30,20 @@ const BarbershopInfo = ({ barbershop }: BarbershopInfoProps) => {
           >
             <ChevronLeftIcon size={18} />
           </Button>
-          <Button
-            size={"icon"}
-            variant={"outline"}
-            className="absolute z-50 top-6 right-6"
-          >
-            <MenuIcon size={18} />
-          </Button>
+          <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              variant={"outline"}
+              size={"icon"}
+              className="absolute z-50 top-6 right-6"
+            >
+              <MenuIcon size={16} />
+            </Button>
+          </SheetTrigger>
+          <SheetContent className="p-0">
+            <SideMenu />
+          </SheetContent>
+        </Sheet>
           <Image
             src={barbershop.imageUrl}
             alt={barbershop.name}
